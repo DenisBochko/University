ea7603a6ec1d9d24cbd01c1532d6f47c07f1bd64
Denis Bochko <reElcy@yandex.ru>
2025-10-29 09:10:37 +0300

    feat: add lec3

--- semester3/programming/lectures/lec3/lec3.cpp
+++ semester3/programming/lectures/lec3/lec3.cpp
@@ -4,17 +4,25 @@ using namespace std;
 /* Алгоритмы сортировки */
 
 void bubble_sort(int* arr, int n);
+void bubble(int* arr, int k);
+void bubble_new(int* x, int k);
+
 void selection_sort(int* arr, int n);
 void reverse_bubble_sort(int* arr, int n);
 
+void Enter_array(int k, int* x);
+void Copy(int k, int* x, int* y);
+void Print(int k, int* x);
+
 int main() {
-    int arr[10] = {4, -12, 0, 5, 13, -33, 6, 10, 0, 17};
+    const int SIZE = 10;
+    int arr[SIZE];
 
-    reverse_bubble_sort(arr, 10);
+    Enter_array(SIZE, arr);
 
-    for (int i = 0; i < 10; i++) {
-        cout << arr[i] << " ";
-    }
+    bubble_new(arr, SIZE);
+
+    Print(SIZE, arr);
 
     return 0;
 }
@@ -56,5 +64,120 @@ void selection_sort(int* arr, int n) {
         int b = arr[i];
         arr[i] = arr[mn_idx];
         arr[mn_idx] = b;
+    }
+}
+
+
+// На Лабораторной делали.
+void bubble(int* x, int k) {
+    int i, j, c = 0, p = 0, buf;
+    for (i = 0; i < k - 1; i++) // Сколько проходов нужно сделать
+        for (j = k - 1; j > i; j--) { // Цикл просмотра
+            c++;
+            if (x[j] < x[j - 1]) {
+                p++;
+                buf = x[j];
+                x[j] = x[j - 1];
+                x[j - 1] = buf;
+            }
+        }
+    printf("c = %d; p = %d; \n", c, p);
+}
+
+/*
+2 6 7 0 9 1 5 3
+            3 5
+        1 9
+    0 7
+  0 6
+0 2
+
+0 2 6 7 1 9 3 5       
+          3 9
+      1 7
+    1 6
+  1 2
+
+0 1 2 6 7 3 9 5
+            5 9
+        3 7
+      3 6
+
+0 1 2 3 6 7 5 9
+          5 7
+        5 6
+
+0 1 2 3 5 6 7 9
+
+0 1 2 3 5 6 7 9
+
+0 1 2 3 5 6 7 9
+
+0 1 2 3 5 6 7 9
+*/
+
+void bubble_new(int* x, int k) {
+    int i = 0, j, m = 0, c = 0, p = 0, buf;
+    do {
+        m = i;
+        for (j = k - 1; j > m; j--) {
+            c++;
+            if (x[j - 1] > x[j]) {
+                i = j; // i - индекс элемента, который последний учаввствовал в перестановке
+                p++;
+                buf = x[j];
+                x[j] = x[j - 1];
+                x[j - 1] = buf;
+            }
+        }
+    } while (i - m);
+    printf("c = %d; p = %d; \n", c, p);
+}
+/*
+2 6 7 0 9 1 5 3
+            3 5
+        1 9
+    0 7
+  0 6
+0 2
+
+m = 0
+0 2 6 7 1 9 3 5
+          3 9
+      1 7
+    1 6
+  1 2
+
+m = 1
+0 1 2 6 7 3 9 5
+            5 9
+          3 5
+        3 7
+      3 6
+    2 3
+
+m = 2
+0 1 2 3 6 7 5 9
+          5 7
+        5 6
+      3 5
+
+m = 3
+0 1 2 3 5 6 7 9
+*/
+
+void Enter_array(int k, int* x) {
+    int i;
+    for (i = 0; i < k; i++) x[i] = rand() % 100;
+}
+
+void Copy(int k, int* x, int* y) {
+    int i;
+    for (i = 0; i < k; i++) x[i] = y[i];
+}
+
+void Print(int k, int* x) {
+    for (int i = 0; i < k; i++) {
+        cout << x[i] << " ";
     }
 }
