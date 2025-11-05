48806f4c90cdd9c63064532813edb223ee432951
Denis Bochko <reElcy@yandex.ru>
2025-11-05 08:40:39 +0300

    feat: add InsertSort

--- semester3/programming/lectures/lec3/lec3.cpp
+++ semester3/programming/lectures/lec3/lec3.cpp
@@ -3,32 +3,39 @@ using namespace std;
 
 /* Алгоритмы сортировки */
 
-void bubble_sort(int* arr, int n);
+void BubbleSort(int* arr, int n);
 void bubble(int* arr, int k);
-void bubble_new(int* x, int k);
-
-void selection_sort(int* arr, int n);
+void BubbleSort_New(int* x, int k);
+void SelectionSort(int* arr, int n);
 void reverse_bubble_sort(int* arr, int n);
+void InsertSort(int k, int* x);
+void Shella(int size, int* a);
 
 void Enter_array(int k, int* x);
 void Copy(int k, int* x, int* y);
 void Print(int k, int* x);
 
 int main() {
-    const int SIZE = 10;
-    int arr[SIZE];
+    int i, Size = 500, a[1000], b[1000];
+    srand(time(NULL));
+    for (i = 0; i < 5; i++) {
+        cout << " " << i + 1 << endl;
+        Enter_array(Size, a);
+        Copy(Size, b, a);
+        Print(Size, a);
+        BubbleSort(a, Size);
 
-    Enter_array(SIZE, arr);
+        Copy(Size, a, b);
+        Print(Size, a);
+        BubbleSort_New(a, Size);
 
-    bubble_new(arr, SIZE);
-
-    Print(SIZE, arr);
+    }
 
     return 0;
 }
 
 // классический алгоритм пузырька
-void bubble_sort(int* arr, int n) {
+void BubbleSort(int* arr, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -53,7 +60,7 @@ void reverse_bubble_sort(int* arr, int n) {
     }
 }
 
-void selection_sort(int* arr, int n) {
+void SelectionSort(int* arr, int n) {
     for (int i = 0; i < n; i++) {
         int mn_idx = i;
 
@@ -116,7 +123,7 @@ void bubble(int* x, int k) {
 0 1 2 3 5 6 7 9
 */
 
-void bubble_new(int* x, int k) {
+void BubbleSort_New(int* x, int k) {
     int i = 0, j, m = 0, c = 0, p = 0, buf;
     do {
         m = i;
@@ -180,4 +187,64 @@ void Print(int k, int* x) {
     for (int i = 0; i < k; i++) {
         cout << x[i] << " ";
     }
+}
+
+// Evoid Shaker (int k, int* x) {
+// int i, t, n1 = 0, n2 = k - 1, ], c = 0, P = 0;
+// do 1 for (i = n2; i › n1; i~~) {
+// c+*;
+// if (x[i - 1] › x[i]) {
+// p**; j = 1;
+// t = x[i - 1]: x[i - 1] = x[1]; x[i] = t;
+// } n1 = 2:
+// for (i = n1 + 1; i <= n2; i++) {
+// C++;
+// if (x[1 - 1] › x[1]) t
+// t = x[1 - 1]:
+// x[i - 1] = x[i];
+// ×[1] = t;
+// j = i; ptt;
+// n2
+// g
+// ]
+// - 1;
+
+void InsertSort(int k, int* x) {
+    int i, j, temp, c = 0, p = 0;
+    for (i = 1; i < k; i++) {
+        c++;
+        if (x[i-1] > x[i]) {
+            temp = x[1]; p++; //поиск места элемента
+            for (j = 1 - 1; j >= 0 && x[j] > temp; j--, c+=2) {
+                p++;
+                x[j + 1] = x[j];
+            } //сдвигаем эл. вправо, пока не дошли до места вст-ки элем-та
+            x[j + 1] = temp; p++;
+        }
+    }
+    Print(k, x);
+    printf("InsertSort: compar=%d, reshuffle=%d\n\n", c, p / 3);
+}
+
+void Shella(int size, int* a) {
+    int i, j, temp, step = size / 2, c = 0, p = 0;
+    while (step) {
+        for (i = 0; i + step < size; i++) {
+            c++;
+            if (a[i] > a[i + step]) {
+                j = i; // будем идти начиная с i-ого элемента
+                temp = a[i + step]; p++;
+                while (j >= 0 && a[j] > temp) {
+                    // пока не пришли к началу массива и пока рассматриваем
+                    // элемен больше элемента, находящегося на расстоянии шаг
+                    c++; p++;
+                    a[j + step] = a[j];
+                    j -= step;
+                }
+                a[j + step] = temp; p++;
+            }
+        }   step /= 2; // уменьшаем шаг
+    } // while step
+    Print(size, a);
+    printf("SortShella: compar=%d, reshuffle=%d\n\n", c, p / 3);
 }
