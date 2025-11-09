1602ac328ccb9f30c9a70a4f4b6270a3dc03443d
reElcy <reElcy@yandex.ru>
2025-11-10 02:46:59 +0300

    wip

--- semester3/programming/lectures/lec3/lec3.cpp
+++ semester3/programming/lectures/lec3/lec3.cpp
@@ -6,9 +6,13 @@ using namespace std;
 void BubbleSort(int* arr, int n);
 void bubble(int* arr, int k);
 void BubbleSort_New(int* x, int k);
-void SelectionSort(int* arr, int n);
+// void SelectionSort(int* arr, int n);
 void reverse_bubble_sort(int* arr, int n);
+
 void InsertSort(int k, int* x);
+void SelectionSort(int k, int* x);
+void Shaker(int k, int* x);
+
 void Shella(int size, int* a);
 
 void Enter_array(int k, int* x);
@@ -23,12 +27,23 @@ int main() {
         Enter_array(Size, a);
         Copy(Size, b, a);
         Print(Size, a);
-        BubbleSort(a, Size);
+        Shaker(Size, a);
 
-        Copy(Size, a, b);
-        Print(Size, a);
-        BubbleSort_New(a, Size);
+        // Copy(Size, a, b);
+        // Print(Size, a);
+        // BubbleSort_New(a, Size);
+        
+        // Copy(Size, a, b);
+        // Print(Size, a);
+        // BubbleSort_New(a, Size);
 
+        // Copy(Size, a, b);
+        // Print(Size, a);
+        // BubbleSort_New(a, Size);
+
+        // Copy(Size, a, b);
+        // Print(Size, a);
+        // BubbleSort_New(a, Size);
     }
 
     return 0;
@@ -60,19 +75,19 @@ void reverse_bubble_sort(int* arr, int n) {
     }
 }
 
-void SelectionSort(int* arr, int n) {
-    for (int i = 0; i < n; i++) {
-        int mn_idx = i;
+// void SelectionSort(int* arr, int n) {
+//     for (int i = 0; i < n; i++) {
+//         int mn_idx = i;
 
-        for (int j = i; j < n; j++) {
-            if (arr[j] < arr[mn_idx]) mn_idx = j;
-        } 
+//         for (int j = i; j < n; j++) {
+//             if (arr[j] < arr[mn_idx]) mn_idx = j;
+//         } 
 
-        int b = arr[i];
-        arr[i] = arr[mn_idx];
-        arr[mn_idx] = b;
-    }
-}
+//         int b = arr[i];
+//         arr[i] = arr[mn_idx];
+//         arr[mn_idx] = b;
+//     }
+// }
 
 
 // На Лабораторной делали.
@@ -209,22 +224,22 @@ void Print(int k, int* x) {
 // ]
 // - 1;
 
-void InsertSort(int k, int* x) {
-    int i, j, temp, c = 0, p = 0;
-    for (i = 1; i < k; i++) {
-        c++;
-        if (x[i-1] > x[i]) {
-            temp = x[1]; p++; //поиск места элемента
-            for (j = 1 - 1; j >= 0 && x[j] > temp; j--, c+=2) {
-                p++;
-                x[j + 1] = x[j];
-            } //сдвигаем эл. вправо, пока не дошли до места вст-ки элем-та
-            x[j + 1] = temp; p++;
-        }
-    }
-    Print(k, x);
-    printf("InsertSort: compar=%d, reshuffle=%d\n\n", c, p / 3);
-}
+// void InsertSort(int k, int* x) {
+//     int i, j, temp, c = 0, p = 0;
+//     for (i = 1; i < k; i++) {
+//         c++;
+//         if (x[i-1] > x[i]) {
+//             temp = x[1]; p++; //поиск места элемента
+//             for (j = 1 - 1; j >= 0 && x[j] > temp; j--, c+=2) {
+//                 p++;
+//                 x[j + 1] = x[j];
+//             } //сдвигаем эл. вправо, пока не дошли до места вст-ки элем-та
+//             x[j + 1] = temp; p++;
+//         }
+//     }
+//     Print(k, x);
+//     printf("InsertSort: compar=%d, reshuffle=%d\n\n", c, p / 3);
+// }
 
 void Shella(int size, int* a) {
     int i, j, temp, step = size / 2, c = 0, p = 0;
@@ -247,4 +262,68 @@ void Shella(int size, int* a) {
     } // while step
     Print(size, a);
     printf("SortShella: compar=%d, reshuffle=%d\n\n", c, p / 3);
+}
+
+// кинула Света
+
+void InsertSort(int k, int* x) {
+    int i, j, temp, c = 0, p = 0;
+    for (i = 1; i < k; i++, c++) {
+        if (x[i - 1] > x[i]) {
+            temp = x[i];
+            p++; // поиск места элемента
+            for (j = i - 1; j >= 0 && x[j] > temp; j--, c += 2) {
+                p++;
+                x[j + 1] = x[j]; // сдвигаем элемент вправо, пока не дошли до места вставки
+            }
+            x[j + 1] = temp;
+            p++;
+        }
+    }
+    Print(k, x);
+    printf("InsertSort: compar=%d, reshuffle=%d\n", c, p / 3);
+}
+
+void SelectionSort(int k, int* x) {
+    int i, j, im, temp, c = 0, p = 0, p1 = 0;
+    for (i = 0; i < k - 1; i++) {
+        im = i; // начальное значение индекса минимального элемента
+        for (j = i + 1; j < k; j++) {
+            c++;
+            if (x[j] < x[im]) {
+                im = j;
+                p1++;
+            }
+        }
+        temp = x[i];
+        x[i] = x[im];
+        x[im] = temp;
+    }
+    Print(k, x);
+    printf("SelectionSort: compar=%d, reshuffle=%d\n\n", c, k-1+p1 / 3);
+}
+
+void Shaker(int k, int* x) {
+    int i, t, n1 = 0, n2 = k - 1, j, c = 0, p = 0;
+    do {
+        for (i = n2; i > n1; i--) {
+            c++;
+            if (x[i - 1] > x[i]) {
+                p++; j = i;
+                t = x[i - 1]; x[i - 1] = x[i]; x[i] = t;
+            }
+        } n1 = j;
+        for (i = n1 + 1; i <= n2; i++) {
+            c++;
+            if (x[i - 1] > x[i]) {
+                t = x[i - 1];
+                x[i - 1] = x[i];
+                x[i] = t;
+                j = i; p++;
+            }
+        }
+        n2 = j - 1;
+    } while (n1 < n2);
+    Print(k, x);
+    printf("Shaiker: compar=%d, reshuffle=%d\n\n", c, p);
 }
